9d4b5177529d349c55ed1345a93c37044f78eff5
david kim <dkreliable@gmail.com>
2025-03-20 12:14:29 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Nav from "./components/Nav";
-import Landing from "./components/landing.jsx";
+
 import "./app.css";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured.jsx";
-import Discounted from "./components/Discounted.jsx";
-import Explore from "./components/Explore.jsx";
+
 import Footer from "./components/Footer.jsx";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
 import { books } from "./data";
@@ -15,14 +12,14 @@ import BookInfo from "./pages/BookInfo.jsx";
 function App() {
   return (
     <Router>
-      {/* <div> */}
+      <div>
         <Nav />
-       
+
         <Route path="/" exact component={Home} />
         <Route path="/books" render={() => <Books books={books} />} />
         <Route path="/books/1" render={() => <BookInfo books={books} />} />
         <Footer />
-      {/* </div> */}
+      </div>
     </Router>
   );
 }
