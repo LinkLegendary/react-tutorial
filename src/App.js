58a6bd9855ee8c32758717d03d35c47998ca5690
david kim <dkreliable@gmail.com>
2025-03-22 17:37:23 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import "./app.css";
 
 import Footer from "./components/Footer.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Home";
 import Books from "./pages/Books.jsx";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+function addTocart(book){
+  setCart([...cart, book])
+}
+
+useEffect(() => {
+    console.log(cart)
+}, [cart])
+
+
+
+
+
+
+ 
+
   return (
     <Router>
       <div>
         <Nav />
 
         <Route path="/" exact component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addTocart={addTocart} />}
+        />
+        <Route path="/cart" render={() => <Cart books={books} />} />
+
         <Footer />
       </div>
     </Router>
@@ -25,3 +49,45 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // function addTocart(book) {
+  //   const dupeItem = cart.find((item) => +item.id === +book.id);
+  //   if (dupeItem) {
+  //     //  dupeItem.quantity += 1;
+  //     setCart(
+  //       cart.map((item) => {
+  //         if (item.id === dupeItem.id) {
+  //           return {
+  //             ...item,
+  //             quantity: item.quantity + 1,
+  //           };
+  //         } else {
+  //           return item;
+  //         }
+  //       })
+  //     );
+  //   }
+  //   else {
+  //     setCart([...cart, {...book, quantity: 1}])
+  //   }
+
+  //   console.log(dupeItem);
+  //   // setCart([...cart, { ...book, quantity: 1 }]);
+  // }
+
+  // useEffect(() => {
+  //   //  console.log(cart);
+  // }, [cart]);
